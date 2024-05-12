a39e94473240d7ed8ceff72f49280a510794506c
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-12 18:12:09 -0300

    Armazenando a palavra da forca

--- forca.py
+++ forca.py
@@ -1,3 +1,7 @@
+import os
+import random
+os.system('cls')
+
 # Desafio: Jogo da Forca
 
 # O programa deve seguir as seguintes diretrizes:
@@ -10,3 +14,21 @@
 # Mostrar ao jogador a palavra parcialmente preenchida, as letras já tentadas e o número de erros.
 # Permitir que o jogador continue tentando até adivinhar a palavra ou exceder um número máximo de erros (por exemplo, 6 erros para desenhar a forca completa).
 # Dar ao jogador a opção de jogar novamente após o término do jogo.
+
+palavras_forca = ['Flamengo', 'Corinthians', 'Palmeiras', 'Vasco','Fluminense', 'Gremio', 'Cruzeiro', 'Sao Paulo', 'Santos', 'Internacional']
+armazena_escolha = []
+
+input('Seja Bem Vindo a o Jogo da Forca dos times Brasileiros!\nIniciar... ')
+
+palavra_escolhida = random.choice(palavras_forca).upper()
+print()
+
+for letra in palavra_escolhida:
+    armazena_escolha.append(letra)
+    print(f'{letra}', end='-')
+
+print(armazena_escolha)
+
+for letra_palavra in armazena_escolha:
+    print('_', end='-')
+    letra_usuario = input('Digite uma letra: ').upper()
