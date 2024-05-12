819e6b8f4e031e769fc5dafba697d85175fac131
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-12 20:33:20 -0300

    Implementei os loops para verificar a letra fornecida pelo usuario

--- forca.py
+++ forca.py
@@ -15,12 +15,45 @@ os.system('cls')
 # Permitir que o jogador continue tentando até adivinhar a palavra ou exceder um número máximo de erros (por exemplo, 6 erros para desenhar a forca completa).
 # Dar ao jogador a opção de jogar novamente após o término do jogo.
 
-palavras_forca = ['Flamengo', 'Corinthians', 'Palmeiras', 'Vasco','Fluminense', 'Gremio', 'Cruzeiro', 'Sao Paulo', 'Santos', 'Internacional']
+palavras_forca = ['Flamengo', 'Corinthians', 'Palmeiras', 'Vasco','Fluminense', 'Gremio', 'Cruzeiro', 'Santos', 'Internacional']
+chances = 5
+armazena_forca = []
+verifica = []
 
-armazena_escolha = []
 
-def introducao():
-    input('Seja Bem Vindo a o Jogo da Forca dos times Brasileiros!\nIniciar... ')
+input('Seja Bem Vindo a o Jogo da Forca dos times Brasileiros!\nIniciar... ')
 
-if __name__ in '__main__':
-    pass
+forca = random.choice(palavras_forca).upper()
+print(forca)
+
+
+for letra in forca:
+    armazena_forca.append(letra)
+
+for v in armazena_forca:
+    verifica.append('_')
+
+while chances > 0:
+    for i in range(0,chances):
+        print(verifica)
+        chute_usuario = input('Digite uma letra: ').upper()
+
+        if chute_usuario in armazena_forca:
+            posicao_letra = armazena_forca.index(chute_usuario) + 1
+            print(posicao_letra)
+            
+            for under in verifica:
+                verifica[posicao_letra - 1] = chute_usuario
+                
+            print('ACERTOU')
+
+        else:
+            chances = chances - 1
+            print(f'ERROU, RESTAM {chances} de 5')
+
+else:
+    print('FIM DE JOGO! VOCÊ PERDEU')
+
+
+print(armazena_forca)
+# if __name__ in '__main__':
